dfa4a51c75c1807ecf308337af5a3e1d4da5ed7e
dawlub <dawid.luberaa@gmail.com>
2023-05-05 02:31:03 +0200

    Connection of html files with php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'Hi there 👋';
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('login', 'DefaultController');
+Routing::get('registration', 'DefaultController');
+Routing::get('dashboard', 'DefaultController');
+Routing::run($path);
